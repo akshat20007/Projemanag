00c1bb22c527ad40565b76523d2932143231705a
akshat <akshat.b2007@gmail.com>
2020-10-30 00:00:41 +0530

    Done till recycler view update

--- app/src/main/java/com/example/projemanage/activities/MainActivity.kt
+++ app/src/main/java/com/example/projemanage/activities/MainActivity.kt
@@ -5,22 +5,28 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.projemanage.R
+import com.example.projemanage.adapters.BoardItemAdapter
 import com.example.projemanage.firebase.FirestoreClass
+import com.example.projemanage.models.Board
 import com.example.projemanage.models.User
 import com.example.projemanage.utils.Constants
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.main_content.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     companion object{
         const val MY_PROFILE_REQUEST_CODE: Int = 11
+        const val CREATE_BOARD_REQUEST_CODE: Int = 12
     }
 
     lateinit var mUserName : String
@@ -33,13 +39,31 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 
         nav_view.setNavigationItemSelectedListener (this)
 
-        FirestoreClass().loadUserData(this)
+        FirestoreClass().loadUserData(this, true)
 
         fab_create_board.setOnClickListener {
             val intent = (Intent(this,
                 CreateBoardActivity::class.java))
             intent.putExtra(Constants.NAME, mUserName)
-            startActivity(intent)
+            startActivityForResult(intent, CREATE_BOARD_REQUEST_CODE)
+        }
+    }
+
+    fun populateBoardListUI(boardsList: ArrayList<Board>){
+        hideProgressDialog()
+
+        if (boardsList.size > 0){
+            rv_boards_list.visibility = View.VISIBLE
+            txt_no_boards_available.visibility = View.GONE
+
+            rv_boards_list.layoutManager = LinearLayoutManager(this@MainActivity)
+            rv_boards_list.setHasFixedSize(true)
+
+            val adapter = BoardItemAdapter(this, boardsList)
+            rv_boards_list.adapter = adapter
+        }else{
+            rv_boards_list.visibility = View.GONE
+            txt_no_boards_available.visibility = View.VISIBLE
         }
     }
 
@@ -71,7 +95,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         }
     }
 
-    fun updateNavigationUserDetails(user: User){
+    fun updateNavigationUserDetails(user: User, readBoardsList: Boolean){
 
         mUserName = user.name
 
@@ -83,6 +107,11 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
             .into(nav_user_image)
 
         txt_username.text = user.name
+
+        if (readBoardsList){
+            showprogressDialog(resources.getString(R.string.please_wait))
+            FirestoreClass().getBoardsList(this )
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -90,7 +119,11 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         if (resultCode == Activity.RESULT_OK
             && requestCode == MY_PROFILE_REQUEST_CODE){
             FirestoreClass().loadUserData(this)
-        }else{
+        }else if (resultCode == Activity.RESULT_OK
+            && requestCode == CREATE_BOARD_REQUEST_CODE){
+            FirestoreClass().getBoardsList(this)
+        }
+        else{
             Log.e("cancelled","cancelled")
         }
     }
--- app/src/main/java/com/example/projemanage/firebase/FirestoreClass.kt
+++ app/src/main/java/com/example/projemanage/firebase/FirestoreClass.kt
@@ -47,6 +47,24 @@ class FirestoreClass {
             }
     }
 
+    fun getBoardsList(activity: MainActivity){
+        mFireStore.collection(Constants.BOARDS)
+            .whereArrayContains(Constants.assignedTo, getCurrentUserid())
+            .get()
+            .addOnSuccessListener {
+                document->
+                Log.e(activity.javaClass.simpleName, document.documents.toString())
+                val boardlist: ArrayList<Board> = ArrayList()
+                for(i in document.documents){
+                    val board = i.toObject(Board::class.java)!!
+                    board.documentId = i.id
+                    boardlist.add(board)
+                }
+
+                activity.populateBoardListUI(boardlist)
+            }
+    }
+
     fun updateUserProfileData(activity: MyProfileActivity, userHashMap: HashMap<String, Any>){
         mFireStore.collection(Constants.USERS)
             .document(getCurrentUserid())
@@ -69,7 +87,7 @@ class FirestoreClass {
             }
     }
 
-    fun loadUserData(activity: Activity){
+    fun loadUserData(activity: Activity, readBoardsList: Boolean = false){
         mFireStore.collection(Constants.USERS)
             .document(getCurrentUserid())
             .get()
@@ -84,7 +102,7 @@ class FirestoreClass {
                     }
                     is MainActivity -> {
                         if (loggedInUser != null) {
-                            activity.updateNavigationUserDetails(loggedInUser)
+                            activity.updateNavigationUserDetails(loggedInUser, readBoardsList )
                         }
                     }
                     is MyProfileActivity -> {
